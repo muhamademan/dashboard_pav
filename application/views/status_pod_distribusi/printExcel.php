696c1fca526ba55cadc0f1c79a8d3315390d867b
muhamademan <muhamad.eman8@gmail.com>
2023-04-17 15:34:25 +0700

    modifikasi view print file Excel

--- application/views/status_pod_distribusi/printExcel.php
+++ application/views/status_pod_distribusi/printExcel.php
@@ -1,16 +1,8 @@
 <?php
-
-// use PhpOffice\PhpSpreadsheet\Spreadsheet;
-// use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
-require 'vendor/autoload.php';
-
-
 // require_once 'path/to/vendor/autoload.php';
 
 use PhpOffice\PhpSpreadsheet\Spreadsheet as spreadsheet; // instead PHPExcel
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx as xlsx; // Instead PHPExcel_Writer_Excel2007
-// use PhpOffice\PhpSpreadsheet\Spreadsheet;
-// use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 use PhpOffice\PhpSpreadsheet\Worksheet\Drawing as drawing; // Instead PHPExcel_Worksheet_Drawing
 use PhpOffice\PhpSpreadsheet\Style\Alignment as alignment; // Instead PHPExcel_Style_Alignment
 use PhpOffice\PhpSpreadsheet\Style\Fill as fill; // Instead PHPExcel_Style_Fill
@@ -20,36 +12,104 @@ use PhpOffice\PhpSpreadsheet\IOFactory as io_factory; // Instead PHPExcel_IOFact
 
 $spreadsheet = new Spreadsheet();
 $sheet = $spreadsheet->getActiveSheet();
-$sheet->setCellValue('A1', 'STATUS POD DISTRIBUSI');
-$sheet->setCellValue('A2', $ket);
-$sheet->setCellValue('A4', $tgl_cetak);
-$sheet->setCellValue('A5', 'NO');
-$sheet->setCellValue('B5', 'PIC PAV');
-$sheet->setCellValue('C5', 'TANGGAL PERMINTAAN');
-$sheet->setCellValue('D5', 'TANGGAL PENGGUNAAN');
-$sheet->setCellValue('E5', 'PIC REQUEST');
-$sheet->setCellValue('F5', 'DEPARTEMENT');
-$sheet->setCellValue('G5', 'NOMOR SPSM');
-$sheet->setCellValue('H5', 'REQUEST CABANG');
-$sheet->setCellValue('I5', 'REGIONAL');
-$sheet->setCellValue('J5', 'PENGGUNAAN ACARA / EVENT');
-$sheet->setCellValue('K5', 'KODE BARANG');
-$sheet->setCellValue('L5', 'MERCHANDISE');
-$sheet->setCellValue('M5', 'QTY');
-$sheet->setCellValue('N5', 'PRICE');
-$sheet->setCellValue('O5', 'TOTAL AMMOUNT');
-$sheet->setCellValue('P5', 'TANGGAL KIRIM');
-$sheet->setCellValue('Q5', 'AWB');
-$sheet->setCellValue('R5', 'TANGGAL TERIMA');
-$sheet->setCellValue('S5', 'NAMA PENERIMA');
-$sheet->setCellValue('T5', 'FOTO DOKUMENTASI');
 
-// $dataVehicle   = $this->M_vehicle->view_all();
+
+$sheet->setCellValue('A1', 'STATUS POD DISTRIBUSI')->mergeCells("A1:C1");
+$sheet->getStyle('A1')->getFont()->setBold(true);
+$sheet->getStyle('A1')->getFont()->setSize(20);
+
+$sheet->setCellValue('A2', $ket)->mergeCells("A2:N2");
+$sheet->getStyle('A2')->getFont()->setBold(true);
+$sheet->getStyle('A2')->getFont()->setSize(12);
+
+$sheet->setCellValue('A4', $tgl_cetak)->mergeCells("A4:D4");
+$sheet->getStyle('A4')->getFont()->setBold(true);
+$sheet->getStyle('A4')->getFont()->setSize(12);
+
+$sheet->setCellValue('A5', 'NO');
+$sheet->getStyle('A5')->getFont()->setBold(true);
+$sheet->getStyle('A5')->getFont()->setSize(12);
+
+$sheet->setCellValue('B5', 'PIC PAV');
+$sheet->getStyle('B5')->getFont()->setBold(true);
+$sheet->getStyle('B5')->getFont()->setSize(12);
+
+$sheet->setCellValue('C5', 'TANGGAL PERMINTAAN');
+$sheet->getStyle('C5')->getFont()->setBold(true);
+$sheet->getStyle('C5')->getFont()->setSize(12);
+
+$sheet->setCellValue('D5', 'TANGGAL PENGGUNAAN');
+$sheet->getStyle('D5')->getFont()->setBold(true);
+$sheet->getStyle('D5')->getFont()->setSize(12);
+
+$sheet->setCellValue('E5', 'PIC REQUEST');
+$sheet->getStyle('E5')->getFont()->setBold(true);
+$sheet->getStyle('E5')->getFont()->setSize(12);
+
+$sheet->setCellValue('F5', 'DEPARTEMENT');
+$sheet->getStyle('F5')->getFont()->setBold(true);
+$sheet->getStyle('F5')->getFont()->setSize(12);
+
+$sheet->setCellValue('G5', 'NOMOR SPSM');
+$sheet->getStyle('G5')->getFont()->setBold(true);
+$sheet->getStyle('G5')->getFont()->setSize(12);
+
+$sheet->setCellValue('H5', 'REQUEST CABANG');
+$sheet->getStyle('H5')->getFont()->setBold(true);
+$sheet->getStyle('H5')->getFont()->setSize(12);
+
+$sheet->setCellValue('I5', 'REGIONAL');
+$sheet->getStyle('I5')->getFont()->setBold(true);
+$sheet->getStyle('I5')->getFont()->setSize(12);
+
+$sheet->setCellValue('J5', 'PENGGUNAAN ACARA / EVENT');
+$sheet->getStyle('J5')->getFont()->setBold(true);
+$sheet->getStyle('J5')->getFont()->setSize(12);
+
+$sheet->setCellValue('K5', 'KODE BARANG');
+$sheet->getStyle('K5')->getFont()->setBold(true);
+$sheet->getStyle('K5')->getFont()->setSize(12);
+
+$sheet->setCellValue('L5', 'MERCHANDISE');
+$sheet->getStyle('L5')->getFont()->setBold(true);
+$sheet->getStyle('L5')->getFont()->setSize(12);
+
+$sheet->setCellValue('M5', 'QTY');
+$sheet->getStyle('M5')->getFont()->setBold(true);
+$sheet->getStyle('M5')->getFont()->setSize(12);
+
+$sheet->setCellValue('N5', 'PRICE');
+$sheet->getStyle('N5')->getFont()->setBold(true);
+$sheet->getStyle('N5')->getFont()->setSize(12);
+
+$sheet->setCellValue('O5', 'TOTAL AMMOUNT');
+$sheet->getStyle('O5')->getFont()->setBold(true);
+$sheet->getStyle('O5')->getFont()->setSize(12);
+
+$sheet->setCellValue('P5', 'TANGGAL KIRIM');
+$sheet->getStyle('P5')->getFont()->setBold(true);
+$sheet->getStyle('P5')->getFont()->setSize(12);
+
+$sheet->setCellValue('Q5', 'AWB');
+$sheet->getStyle('Q5')->getFont()->setBold(true);
+$sheet->getStyle('Q5')->getFont()->setSize(12);
+
+$sheet->setCellValue('R5', 'TANGGAL TERIMA');
+$sheet->getStyle('R5')->getFont()->setBold(true);
+$sheet->getStyle('R5')->getFont()->setSize(12);
+
+$sheet->setCellValue('S5', 'NAMA PENERIMA');
+$sheet->getStyle('S5')->getFont()->setBold(true);
+$sheet->getStyle('S5')->getFont()->setSize(12);
+
+$sheet->setCellValue('T5', 'FOTO DOKUMENTASI');
+$sheet->getStyle('T5')->getFont()->setBold(true);
+$sheet->getStyle('T5')->getFont()->setSize(12);
 
 // if (!empty($dataVehicle)) {
 $no = 1;
 $baris = 6;
-foreach ($dataVehicle as $data) {
+foreach ($data_pod as $data) {
     $sheet->setCellValue('A' . $baris, $no++);
     $sheet->setCellValue('B' . $baris, $data['NAMAPAV']);
     $sheet->setCellValue('C' . $baris, $data['TGLPERMINTAAN']);
@@ -70,25 +130,26 @@ foreach ($dataVehicle as $data) {
     $sheet->setCellValue('R' . $baris, $data['TGLDITERIMA']);
     $sheet->setCellValue('S' . $baris, $data['NAMAPENERIMA']);
 
-    // if (file_exists('http://localhost/jne-cbd/uploads/vehicle/' . $data['FOTO'])) {
+    if (file_exists('./uploads/update_dokumentasi/' . $data['FOTOPENERIMA'])) {
+        $objDrawing = new drawing();
+        $objDrawing->setPath('./uploads/update_dokumentasi/' . $data['FOTOPENERIMA']);
+        $objDrawing->setCoordinates('T' . $baris);
+        $objDrawing->setWorksheet($sheet);
+        $sheet->getRowDimension($baris)->setRowHeight(120);
+    } else {
+        $sheet->setCellValue('T' . $baris, 'image not found');
+    }
 
-    //     $objDrawing = new PHPExcel_Worksheet_Drawing();
-    //     $objDrawing->setPath('http://localhost/jne-cbd/uploads/vehicle/' . $data['FOTO']);
-    //     $objDrawing->setCoordinates('L' . $baris);
-    //     $objDrawing->setWorksheet($this->excel->getActiveSheet());
-    //     $sheet->getRowDimension($baris)->setRowHeight(120);
-    // } else {
-    //     $sheet->setCellValue('T' . $baris, 'http://localhost/jne-cbd/uploads/vehicle/' . $data['FOTO']);
-    // }
-    $sheet->setCellValue('T' . $baris, base_url('./upload/update_dokumentasi/') . $data['FOTOPENERIMA']);
+    // $sheet->setCellValue('T' . $baris, base_url('uploads/update_dokumentasi/') . $data['FOTOPENERIMA']);
 
 
     $baris++;
 }
 // }
 $writer = new Xlsx($spreadsheet);
-$filename = "All Data POD Distribusi";
+$filename = "Data POD Distribusi";
 
+ob_end_clean();
 header('Content-Type: application/vnd.ms-excel');
 header('Content-Disposition: attachment;filename="' . $filename . '.xlsx"');
 header('Cache-Control: max-age=0');
