963b33469f1e3441359216c2382b771ec86a2201
muhamademan <muhamad.eman8@gmail.com>
2023-05-08 15:01:32 +0700

    undo query

--- application/models/M_home.php
+++ application/models/M_home.php
@@ -6,4 +6,12 @@ class M_home extends CI_Model
     {
         return $this->db->query("SELECT COUNT(KODEVENDOR) FROM PAV_VENDOR")->result_array();
     }
+
+    // public function coba()
+    // {
+    //     $this->db->select('LOKASI');
+    //     $this->db->select_sum('JUMLAH');
+    //     $this->db->get('PAV_BARANGMASUK');
+    //     $this->db->group_by('LOKASI');
+    // }
 }
