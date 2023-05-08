296dfa1a5cc4762c1c43280396662f4ac69bf43f
muhamademan <muhamad.eman8@gmail.com>
2023-05-08 15:01:00 +0700

    menambahkan query hargaMerchan dan lokasi

--- application/controllers/Stok_barangMerchan.php
+++ application/controllers/Stok_barangMerchan.php
@@ -6,6 +6,7 @@ class stok_barangMerchan extends CI_Controller
     public function __construct()
     {
         parent::__construct();
+        $this->load->model('M_home');
     }
 
     public function stok_merchandise()
@@ -14,6 +15,11 @@ class stok_barangMerchan extends CI_Controller
         $data['title'] = 'Stok Barang/Merchandise';
 
         $data['stok_merchan'] = $this->db->get('PAV_BARANGMASUK')->result_array();
+
+        // $data['hargaMerchan'] = $this->db->query("SELECT LOKASI, (SELECT sum(HARGA) FROM PAV_BARANGMASUK)HARGA, (SELECT sum(JUMLAH) FROM PAV_BARANGMASUK) JUMLAH FROM PAV_BARANGMASUK")->result_array();
+        $data['hargaMerchan'] = $this->db->query("SELECT sum(HARGA) as HARGA, sum(JUMLAH) as JUMLAH FROM PAV_BARANGMASUK")->result_array();
+
+        $data['lokasi'] = $this->db->query("SELECT LOKASI, sum(HARGA) AS HARGA, SUM(JUMLAH) AS JUMLAH FROM PAV_BARANGMASUK GROUP BY LOKASI")->result();
 
         $this->load->view('templates/header', $data);
         $this->load->view('templates/sidebar', $data);
